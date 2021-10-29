ca9b050102b3db89655ac77c89f6fe5598ec2219
robiot <robiot@users.noreply.github.com>
2021-10-29 23:05:02 +0300

    Add pipes

--- src/opts.rs
+++ src/opts.rs
@@ -3,5 +3,13 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "zash")]
 pub struct Opts {
-    // No args yet
+    // These ops are just here to prevent errors with applications
+    #[structopt(short, long)]
+    pub interactive: bool,
+
+    #[structopt(short, long)]
+    pub login: bool,
+
+    #[structopt(short, long)]
+    pub command: Option<String>,
 }
